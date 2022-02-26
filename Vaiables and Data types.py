00a297db9f0fa48b02b144aaecef13527212b9a5
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2022-02-26 15:44:33 -0500

    Vaiables and Data types

---
+++ Vaiables and Data types.py
@@ -0,0 +1,10 @@
+character_name = "John"
+character_age = "50"
+isMale = True
+is_male = False
+print("There once was a man named " + character_name + ", ")
+print("he was 35 " + character_age + " years old.")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("BUT DIDN'T LINE BEING " + character_age + ".")
