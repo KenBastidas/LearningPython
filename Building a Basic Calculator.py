b5c65d9acecab1fb043802aed9f5458dcdd4588b
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2022-02-26 19:08:23 -0500

    Building a Basic Calculator

---
+++ Building a Basic Calculator.py
@@ -0,0 +1,14 @@
+#Building a Basic Calculator
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2)
+prom = result/2
+print(result)
+print(prom)
+#part2
+num11 = input("Enter a number: ")
+num22 = input("Enter another number: ")
+result2 = float(num11) + float(num22)
+prom2 = result/2
+print(result2)
+print(prom2)
